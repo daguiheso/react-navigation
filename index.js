ff74073b4de2a21ac230c429c1adebf44351525a
Daniel H <daguiheso@gmail.com>
2020-03-20 17:57:30 -0500

    opening a full-screen modal

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import Modal from './Modal';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Modal);
